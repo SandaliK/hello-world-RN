0ecadcf33458740fa015bb66c90ecccad80ec202
Sandali Kulanika <skulanika@gmail.com>
2021-02-10 19:47:01 +0530

    Button Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View,Text,ImageBackground} from 'react-native';
+import StyledButton from "../StyledButton";
 import styles from './styles';
 
 
@@ -18,6 +19,19 @@ const CarItem = (props) => {
          <Text style = {styles.mainTitle}>Model S</Text>
          <Text style = {styles.subTitle}>Starting from $69,000</Text>
        </View>
+
+       <StyledButton type = 'primary' 
+       content = {"Custom Order"} 
+       onPress = {() => {
+         console.warn("Custom Order was pressed")
+       }} />
+
+       <StyledButton type = 'secondary' 
+       content = {"Existing Inventory"} 
+       onPress = {() => {
+         console.warn("Existing Inventory was pressed")
+       }} />
+
 
      </View>
     );
