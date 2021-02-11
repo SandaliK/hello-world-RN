98119d7fbe4683c04946ae3dbc90d003688f2124
Sandali Kulanika <skulanika@gmail.com>
2021-02-11 10:20:55 +0530

    Header and FlatList

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import {View,Text,FlatList} from 'react-native';
+import {View,Text,FlatList,Dimensions} from 'react-native';
 import styles from './styles';
 import cars from './cars';
 import CarItem from "../CarItem";
 
 const CarsList = (props) => {
 
-    return
+    return (
 
     <View style = {styles.container}>
         <FlatList
         data = {cars}
-        renderItem = {({item}) => 
-        <CarItem car={item} />
-    }
+        renderItem = {({item}) => <CarItem car={item} />}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment = {'start'}
+        decelerationRate = {'fast'}
+        snapToInterval = {Dimensions.get('window').height}
         />
     </View>
 
+    );
 
 };
 
